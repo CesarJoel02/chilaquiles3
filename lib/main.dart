846e04bc9a893b8a712939584bc6234b9e104097
Cesar joel <cesar.joel.fenix@gmail.com>
2019-07-21 23:03:23 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_spinning_wheel/flutter_spinning_wheel.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final StreamController _dividerController = StreamController<int>();
+
+  dispose() {
+    _dividerController.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Color(0xffDDC3FF), elevation: 0.0),
+      backgroundColor: Color(0xffDDC3FF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinningWheel(
+              Image.asset('assets/roulette-8-300.png'),
+              width: 310,
+              height: 310,
+              initialSpinAngle: _generateRandomAngle(),
+              spinResistance: 0.6,
+              canInteractWhileSpinning: false,
+              dividers: 4,
+              onUpdate: _dividerController.add,
+              onEnd: _dividerController.add,
+              secondaryImage:
+                  Image.asset('assets/roulette-center-300.png'),
+              secondaryImageHeight: 110,
+              secondaryImageWidth: 110,
+            ),
+            SizedBox(height: 30),
+            StreamBuilder(
+              stream: _dividerController.stream,
+              builder: (context, snapshot) =>
+                  snapshot.hasData ? RouletteScore(snapshot.data) : Container(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  double _generateRandomAngle() => Random().nextDouble() * pi * 2;
+}
+
+class RouletteScore extends StatelessWidget {
+  final int selected;
+
+  final Map<int, String> labels = {
+    1: 'Fernanda',
+    2: 'Eduardo',
+    3: 'Rodrigo',
+    4: 'Cesar',
+  };
+
+  RouletteScore(this.selected);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('${labels[selected]}',
+        style: TextStyle(fontStyle: FontStyle.italic, fontSize: 24.0));
+  }
+}
